adb037092a7ed08544203bec2dcdd4872343a009
Mikael <prizelive@live.dk>
2019-08-09 18:27:51 +0200

    Added append proxies from URL and append proxies from reader

--- Manager.go
+++ Manager.go
@@ -3,8 +3,10 @@ package proxy
 import (
 	"bufio"
 	"errors"
+	"io"
 	"math/rand"
 	"net"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -44,18 +46,13 @@ func (m *Manager) Alive() ([]*Proxy, error) {
 	return alive, nil
 }
 
-// AppendProxiesFromFile appends proxies from a file to the proxy manager.
+// AppendProxiesFromReader appends proxies from an io.Reader using a scanner.
 // First argument specifies the protocol of the proxies.
 // These should be proxy.HTTP, proxy.SOCKS4 or proxy.SOCKS5.
 // HTTPS should be specified as HTTP. SOCKS4a should be specified as SOCKS4.
-// Each line of the file should be a new proxy in the form: host:port
-func (m *Manager) AppendProxiesFromFile(protocol int, filename string) error {
-	file, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
+// Each token of the scanner should be a new proxy in the form: host:port
+func (m *Manager) AppendProxiesFromReader(protocol int, r io.Reader) error {
+	scanner := bufio.NewScanner(r)
 
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
@@ -70,5 +67,33 @@ func (m *Manager) AppendProxiesFromFile(protocol int, filename string) error {
 			m.Proxies = append(m.Proxies, p)
 		}
 	}
-	return nil
+	return scanner.Err()
+}
+
+// AppendProxiesFromFile appends proxies from a file to the proxy manager.
+// First argument specifies the protocol of the proxies.
+// The content of the file is processed by AppendProxiesFromReader.
+// Each line of the file should be a new proxy in the form: host:port
+func (m *Manager) AppendProxiesFromFile(protocol int, filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	err = m.AppendProxiesFromReader(protocol, file)
+	return err
+}
+
+// AppendProxiesFromURL appends proxies from an URL. The ressource should be
+// available through a blank HTTP GET request.
+// First argument specifies the protocol of the proxies.
+// The body of the response is processed by AppendProxiesFromReader.
+func (m *Manager) AppendProxiesFromURL(protocol int, url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	err = m.AppendProxiesFromReader(protocol, resp.Body)
+	return err
 }
